bf1298f84093d5fcd203b6d430d272ee58e1eb3e
Rolando Espinoza La fuente <darkrho@gmail.com>
2010-01-31 11:49:33 -0400

    minesweeper: process fields from standard input

--- minesweeper/python/minesweeper.py
+++ minesweeper/python/minesweeper.py
@@ -91,3 +91,59 @@ class Field(object):
 
         return result
 
+
+if __name__ == '__main__':
+    """
+    Read field from standard input and display result
+    Usage: $ cat input | python minesweeper.py
+    """
+    import fileinput
+    import sys
+    nlines = 0
+    mcols = 0
+    field_input = None
+    field_count = 0
+    expect_size_line = True
+    for line in fileinput.input():
+        # strip \n
+        line = line.strip()
+
+        if expect_size_line:
+            # expect two integers
+            nlines, mcols = map(int, line.split())
+            if nlines > 0:
+                # Read fields in next loop
+                expect_size_line = False
+                # initialize field
+                field_input = list()
+                linecount = nlines
+                field_count += 1
+        else:
+            # append line as list/row
+            field_input.append(list(line))
+            # pop line
+            linecount -= 1
+
+            if linecount == 0:
+                # process next field
+                expect_size_line = True
+
+                # Read complete display result
+                field = Field(field_input)
+
+                if not field.is_empty():
+                    result = field.resolve()
+
+                    if field_count > 1:
+                        sys.stdout.write("\n")
+
+                    sys.stdout.write("Field #%d\n" % field_count)
+                    for row in result:
+                        for cell in row:
+                            sys.stdout.write(cell)
+                        sys.stdout.write("\n")
+
+
+
+
+
